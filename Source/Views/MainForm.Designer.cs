9d611ea83ac1699b3a7ce45106cbafc4c540401d
Van Dung Dev <vincentcode402112@gmail.com>
2024-10-27 15:43:31 +0700

    layout

---
+++ Source/Views/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Source.Views
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(192, 627);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(192, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1112, 77);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(192, 550);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1112, 77);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(192, 77);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1112, 473);
+            panel4.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1304, 627);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "MainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+    }
+}

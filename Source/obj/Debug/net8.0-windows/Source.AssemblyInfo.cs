f19e3b4b1f019b19ff66b17d55a22de992dea551
TeeNoiz04 <duoc14525@gmail.com>
2024-10-31 07:28:22 +0700

    Abc

--- Source/obj/Debug/net8.0-windows/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net8.0-windows/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697907285663f1088171ba0400ed6edb68da69fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264a18f9d1a1cdce2402df3c68b8eb3d79ce7250")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

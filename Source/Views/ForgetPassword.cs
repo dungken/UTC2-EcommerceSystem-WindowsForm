1a00d9c30e5d0865200b71832ced0c0c7a81ae12
Oanh Phạm <opham414@gmail.com>
2025-01-03 08:38:14 +0700

    create forget/change password

---
+++ Source/Views/ForgetPassword.cs
@@ -0,0 +1,73 @@
+﻿using Source.Dtos.Account;
+using Source.Service;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace Source.Views
+{
+    public partial class ForgetPassword : Form
+    {
+        // Biến thành viên để lưu trạng thái OTP và email
+        private string userEmail;
+        private readonly AccountService _accountService;
+
+        public ForgetPassword()
+        {
+            InitializeComponent();
+            _accountService = new AccountService();
+            txtNewPassword.Hide();
+            btnConfirm.Hide();
+            label3.Hide();
+            panel4.Hide();
+        }
+        private async Task ForgotPasswordAsync()
+        {
+            ForgotPasswordDto forgotPasswordDto = new ForgotPasswordDto
+            {
+                Email = userEmail
+            };
+            var response = await _accountService.ForgotPassword(forgotPasswordDto);
+            if (response != null && response.Success)
+            {
+                MessageBox.Show($"Forgot Password Request Successful\nMessage: {response.Message}\nReset Link: {response.Data.ResetLink}");
+            }
+            else
+            {
+                MessageBox.Show($"Forgot Password Request Failed\nError: {string.Join(", ", response.Errors)}");
+            }
+        }
+
+        private async Task ResetPasswordAsync()
+        {
+            ResetPasswordDto resetPasswordDto = new ResetPasswordDto
+            {
+                Token = "sampleToken",
+                Email = "test@example.com",
+                NewPassword = "newPassword123"
+            };
+            var response = await _accountService.ResetPassword(resetPasswordDto);
+            if (response != null && response.Success)
+            {
+                MessageBox.Show($"Password Reset Successful\nMessage: {response.Message}");
+            }
+            else
+            {
+                MessageBox.Show($"Password Reset Failed\nError: {string.Join(", ", response?.Errors ?? new List<string>())}");
+            }
+        }
+
+        private async void btnGetLink_Click(object sender, EventArgs e)
+        {
+            userEmail = txtEmail.Text.ToString();
+            await ForgotPasswordAsync();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
--- Source/Views/Login.cs
+++ Source/Views/Login.cs
@@ -1,4 +1,8 @@
-﻿using Source.Dtos.Account;
+﻿using Google.Apis.Auth;
+using Google.Apis.Util;
+using Newtonsoft.Json;
+using Source.Dtos.Account;
+using Source.Models;
 using Source.Service;
 using Source.Utils;
 using Source.Views.Custommer;
@@ -8,6 +12,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +28,7 @@ namespace Source.Views
 
         public static int parentX, parentY;
         private readonly UserService _userService = new UserService();
+        private static readonly HttpClient client = new HttpClient();
         public Login()
         {
             InitializeComponent();
@@ -57,7 +63,7 @@ namespace Source.Views
         private void btnResgister_Click(object sender, EventArgs e)
         {
             openChildForm(new Resgister());
-        }     
+        }
 
         private async void btnLogin_Click(object sender, EventArgs e)
         {
@@ -84,8 +90,8 @@ namespace Source.Views
                     EmailOrUsername = emailOrUsername,
                     Password = password
                 };
-              
-               
+
+
                 var response = await _AccountService.LoginAsync(loginUserDto);
 
 
@@ -108,7 +114,7 @@ namespace Source.Views
                             modalBackground.StartPosition = FormStartPosition.Manual;
                             modalBackground.FormBorderStyle = FormBorderStyle.None;
                             modalBackground.Opacity = 0.50d;
-                            modalBackground.Size = new Size(this.Width, this.Height);
+                            modalBackground.Size = new System.Drawing.Size(this.Width, this.Height);
 
 
                             modalBackground.Location = new Point(this.Location.X, this.Location.Y);
@@ -126,6 +132,7 @@ namespace Source.Views
                         {
                             Config.token = response.Data.Token;
                             MessageBox.Show("Login successful! ");
+                            
                             // Handle successful login
                             openChildForm(new MainForm());
                         }
@@ -139,9 +146,10 @@ namespace Source.Views
                         Config.token = response.Data.Token;
                         MessageBox.Show("Login successful! ");
                         // Handle successful login
+                        
                         openChildForm(new MainForm());
                     }
-                 
+
                 }
 
             }
@@ -151,7 +159,122 @@ namespace Source.Views
             }
 
         }
-    
+
+        private async void btnLoginWGoogle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Thông tin Client ID và Client Secret
+                string clientId = "929983322362-n15r2kq4njvpdd32p8nb3hmotlduoq14.apps.googleusercontent.com";
+                string clientSecret = "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";
+                string redirectUri = "http://localhost:8081/";
+
+                // Lấy mã xác thực từ Google
+                string authCode = await GetAuthCodeAsync(clientId, redirectUri);
+
+                // Trao đổi mã xác thực để lấy Access Token và ID Token
+                var tokenResponse = await ExchangeAuthCodeForTokens(authCode, clientId, clientSecret, redirectUri);
+
+                // Đọc ID Token từ phản hồi
+                string idToken = tokenResponse.id_token;
+                if (string.IsNullOrWhiteSpace(idToken))
+                {
+                    throw new Exception("Không lấy được ID Token từ phản hồi của Google.");
+                }
+
+                // Giải mã ID Token để lấy thông tin người dùng
+                var userInfo = DecodeJwt(idToken);
+
+                // Hiển thị thông tin người dùng
+                MessageBox.Show($"Đăng nhập thành công!\nTên: {userInfo["name"]}\nEmail: {userInfo["email"]}");
+
+                // Chuyển đến MainForm
+                this.Hide();
+                new MainForm().Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi đăng nhập với Google: {ex.Message}");
+            }
+        }
+
+        private async Task<dynamic> ExchangeAuthCodeForTokens(string authCode, string clientId, string clientSecret, string redirectUri)
+        {
+            var queryParams = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("code", authCode),
+                new KeyValuePair<string, string>("client_id", clientId),
+                new KeyValuePair<string, string>("client_secret", clientSecret),
+                new KeyValuePair<string, string>("redirect_uri", redirectUri),
+                new KeyValuePair<string, string>("grant_type", "authorization_code")
+            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(queryParams);
+                var response = await client.PostAsync("https://oauth2.googleapis.com/token", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<dynamic>(responseContent);
+                }
+                else
+                {
+                    throw new Exception($"Lỗi khi trao đổi mã xác thực: {response.StatusCode}");
+                }
+            }
+        }
+
+        private async Task<string> GetAuthCodeAsync(string clientId, string redirectUri)
+        {
+            using (var httpListener = new HttpListener())
+            {
+                httpListener.Prefixes.Add(redirectUri);
+                httpListener.Start();
+
+                string authUrl = $"https://accounts.google.com/o/oauth2/v2/auth?client_id={clientId}&response_type=code&scope=openid%20email%20profile&redirect_uri={Uri.EscapeDataString(redirectUri)}&access_type=offline";
+
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = authUrl,
+                    UseShellExecute = true
+                });
+
+                var context = await httpListener.GetContextAsync();
+                string code = context.Request.QueryString["code"];
+                if (string.IsNullOrWhiteSpace(code))
+                {
+                    throw new Exception("Không nhận được mã xác thực từ Google.");
+                }
+
+                using (var writer = new StreamWriter(context.Response.OutputStream))
+                {
+                    writer.WriteLine("<html><body><h1>Đăng nhập thành công!</h1><p>Bạn có thể quay lại ứng dụng.</p></body></html>");
+                }
+
+                context.Response.StatusCode = 200;
+                context.Response.Close();
+
+                return code;
+            }
+        }
+
+        // Giải mã ID Token để lấy thông tin người dùng
+        private Dictionary<string, string> DecodeJwt(string idToken)
+        {
+            System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler handler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(idToken);
+
+            var claims = jwtToken.Claims.ToDictionary(c => c.Type, c => c.Value);
+            return claims;
+        }
+
+        private void lblForget_Click(object sender, EventArgs e)
+        {
+            ForgetPassword forgetPassword = new ForgetPassword();
+            forgetPassword.Show();
+        }
     }
 
 }
